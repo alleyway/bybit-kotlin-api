07c8e23635c6d682d02d881b0125cc5fbc271e50
Michael Lake <mlake@alleywayapps.com>
2023-06-19 20:52:27 +0200

    use enums instead

--- src/main/kotlin/bybit/sdk/rest/order/OrderType.kt
+++ src/main/kotlin/bybit/sdk/rest/order/OrderType.kt
@@ -1,12 +1,12 @@
 package bybit.sdk.rest.order
 
-sealed class OrderType(val value: String) {
-    object Limit : OrderType("Limit")
-    object Market : OrderType("Market")
+enum class OrderType {
+    Limit,
+    Market
 }
 
 
-sealed class Side(val value: String) {
-    object Buy : Side("Buy")
-    object Sell : Side("Sell")
+enum class Side {
+    Buy,
+    Sell
 }
