d42ac2f161f74979fc85079164a81cf1ec6e225f
Michael Lake <mlake@alleywayapps.com>
2024-03-01 16:16:10 +0100

    adding IntervalTime

---
+++ src/main/kotlin/bybit/sdk/shared/IntervalTime.kt
@@ -0,0 +1,11 @@
+package bybit.sdk.shared
+
+enum class IntervalTime(val stringValue: String) {
+    FiveMinutes("5min"),
+    FifteenMinutes("15min"),
+    ThirtyMinutes("30min"),
+    OneHour("1h"),
+    FourHour("4h"),
+    OneDay("1d"),
+}
+
