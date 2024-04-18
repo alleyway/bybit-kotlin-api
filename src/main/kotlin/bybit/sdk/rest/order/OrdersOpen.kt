7eccdd70c14827c66e39e3abb21881f789598b4b
Michael Lake <mlake@alleywayapps.com>
2024-04-18 19:37:22 +0200

    add a lot more to orders open

--- src/main/kotlin/bybit/sdk/rest/order/OrdersOpen.kt
+++ src/main/kotlin/bybit/sdk/rest/order/OrdersOpen.kt
@@ -4,6 +4,9 @@ import bybit.sdk.rest.APIResponseV5Paginatable
 import bybit.sdk.rest.ListResult
 import bybit.sdk.shared.Category
 import bybit.sdk.shared.OrderStatus
+import bybit.sdk.shared.OrderType
+import bybit.sdk.shared.Side
+import bybit.sdk.shared.TimeInForce
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
 import lombok.Builder
@@ -18,8 +21,13 @@ suspend fun ByBitOrderClient.ordersOpen(
             "order",
             "realtime",
         )
-        params.category.let { parameters["category"] = it.toString() }
+        parameters["category"] = params.category.toString()
         params.symbol?.let { parameters["symbol"] = it }
+        params.baseCoin?.let { parameters["baseCoin"] = it }
+        params.orderId?.let { parameters["orderId"] = it }
+        params.orderLinkId?.let { parameters["orderLinkId"] = it }
+        params.openOnly?.let { parameters["openOnly"] = it.toString() }
+        params.orderFilter?.let { parameters["orderFilter"] = it }
         params.limit?.let { parameters["limit"] = it.toString() }
     }, HttpMethod.Get, false)
 
@@ -27,17 +35,60 @@ suspend fun ByBitOrderClient.ordersOpen(
 @Builder
 data class OrdersOpenParams(
     val category: Category,
-    val symbol: String? = "",
+    val symbol: String? = null,
+    val baseCoin: String? = null,
+    val settleCoin: String? = null,
+    val orderId: String? = null,
+    val orderLinkId: String? = null,
+    val openOnly: Int? = null,
+    val orderFilter: String? = null,
     val limit: Int? = 50
-    )
+)
 
 @Serializable
 data class OrdersOpenResultItem(
     val orderId: String,
     val orderLinkId: String,
+    val blockTradeId: String,
+    val symbol: String,
     val price: String,
     val qty: String,
-    val orderStatus: OrderStatus
+    val side: Side,
+    val isLeverage: String,
+    val positionIdx: Int,
+    val orderStatus: OrderStatus,
+    val createType: String,
+    val cancelType: String,
+    val rejectReason: String,
+    val avgPrice: String,
+    val leavesQty: String,
+    val leavesValue: String,
+    val cumExecQty: String,
+    val cumExecValue: String,
+    val cumExecFee: String,
+    val timeInForce: TimeInForce,
+    val orderType: OrderType,
+    val stopOrderType: String,
+    val orderIv: String,
+    val triggerPrice: String,
+    val takeProfit: String,
+    val stopLoss: String,
+    val tpTriggerBy: String,
+    val slTriggerBy: String,
+    val triggerDirection: Int,
+    val triggerBy: String,
+    val lastPriceOnCreated: String,
+    val reduceOnly: Boolean,
+    val closeOnTrigger: Boolean,
+    val smpType: String,
+    val smpGroup: Int,
+    val smpOrderId: String,
+    val tpslMode: String,
+    val tpLimitPrice: String,
+    val slLimitPrice: String,
+    val placeType: String,
+    val createdTime: Long,
+    val updatedTime: Long
 )
 
 @Serializable
