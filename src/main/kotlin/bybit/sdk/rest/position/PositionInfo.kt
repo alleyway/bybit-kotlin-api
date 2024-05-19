a30a06efffb4df4380c6df60dc6a899c46828f38
Michael Lake <mlake@alleywayapps.com>
2024-05-19 14:31:01 +0200

    added a lot more position info items

--- src/main/kotlin/bybit/sdk/rest/position/PositionInfo.kt
+++ src/main/kotlin/bybit/sdk/rest/position/PositionInfo.kt
@@ -2,6 +2,7 @@ package bybit.sdk.rest.position
 
 import bybit.sdk.rest.APIResponseV5
 import bybit.sdk.shared.Category
+import bybit.sdk.shared.PositionStatus
 import bybit.sdk.shared.Side
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
@@ -30,18 +31,35 @@ data class PositionInfoParams(
 
 @Serializable
 data class PositionInfoListResultItem(
+    val positionIdx: Int,
+    val riskId: Int,
+    val riskLimitValue: String,
     val symbol: String,
     val side: Side,
     val size: String,
     val avgPrice: String,
-    val leverage: String,
     val positionValue: String,
-    val positionBalance: String,
     val tradeMode: Int,
-    val liqPrice: String,
+    val positionStatus: PositionStatus,
+    val autoAddMargin: Int,
+    val adlRankIndicator: Int,
+    val leverage: String,
+    val positionBalance: String,
     val markPrice: String,
-    val positionIM: String,
+    val liqPrice: String,
+    val bustPrice: String,
     val positionMM: String,
+    val positionIM: String,
+    val takeProfit: String,
+    val stopLoss: String,
+    val trailingStop: String,
+    val unrealisedPnl: String,
+    val curRealisedPnl: String,
+    val seq: Long,
+    val isReduceOnly: Boolean,
+    val mmrSysUpdatedTime: String,
+    val leverageSysUpdatedTime: String,
+    val sessionAvgPrice: String,
     val createdTime: String,
     val updatedTime: String,
 )
